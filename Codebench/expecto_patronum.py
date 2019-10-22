ee88c5b15667c88658ff2e598a61ddb436d3cd61
fabianabezerra <fabisouza.bezerra@gmail.com>
2019-10-22 13:02:13 -0400

    Update expecto_patronum.py

--- Codebench/expecto_patronum.py
+++ Codebench/expecto_patronum.py
@@ -24,3 +24,21 @@ substituindo a expressão <entrada> pela string fornecida como entrada.
     Saída   : asno nao eh patrono do Harry Potter
  """
 
+def strequal(string1,string2):
+	for i in range (len(string2)):
+		aux = string2.lower()
+	
+	if(len(string1)!=len(string2)):
+		return 1;
+	
+	for i in range (len(string1)):
+		if(string1[i] != aux[i]):
+			return 1;
+	return 0
+	
+entrada = input()
+
+if (strequal("cervo",entrada)==0):
+	print(entrada,"eh patrono do Harry Potter")
+else:
+	print(entrada,"nao eh patrono do Harry Potter")
